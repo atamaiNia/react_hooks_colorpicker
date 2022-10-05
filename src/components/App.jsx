09f33911f70f4d35734e4dacd6de4885eaed5536
atamaiNia <bimatideka@gmail.com>
2022-10-05 16:16:56 +0300

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import ColorPicker from './ColorPicker';
+
+const colorPickerOptions = [
+  { label: 'red', color: '#f44336' },
+  { label: 'green', color: '#4caf50' },
+  { label: 'blue', color: '#2196f3' },
+  { label: 'grey', color: '#607d88' },
+  { label: 'pink', color: '#e91e63' },
+  { label: 'indigo', color: '#3f51b5 ' },
+];
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="Container">
+      <h1>React - color picker</h1>
+      <ColorPicker options={colorPickerOptions} />
     </div>
   );
 };
---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './ColorPicker.css';
+
+class ColorPicker extends Component {
+  state = {
+    activeOptionIdx: 2,
+  };
+
+  setActiveIdx = index => {
+    this.setState({ activeOptionIdx: index });
+  };
+
+  makeOptionClassName = index => {
+    const optionClasses = ['ColorPicker__option'];
+
+    if (index === this.state.activeOptionIdx) {
+      optionClasses.push('ColorPicker__option--active');
+    }
+    return optionClasses.join(' ');
+  };
+
+  render() {
+    const { activeOptionIdx } = this.state;
+    const { options } = this.props;
+    const { label } = options[activeOptionIdx];
+
+    return (
+      <div className="ColorPicker">
+        <h1 className="ColorPicker__title">
+          Вибраний колір: <span>{label}</span>
+        </h1>
+        <div>
+          {options.map(({ label, color }, index) => (
+            <button
+              key={label}
+              className={this.makeOptionClassName(index)}
+              style={{ backgroundColor: color }}
+              onClick={() => this.setActiveIdx(index)}
+            ></button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
